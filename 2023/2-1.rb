a138eeca69dbd00cd24480b0a11a30b2bed20b20
Vedran Rodic <vrodic@gmail.com>
2023-12-02 21:42:12 +0100

    small cleanup

--- 2023/2-1.rb
+++ 2023/2-1.rb
@@ -1,36 +1,29 @@
+# frozen_string_literal: true
+
 availiable_cubes = {
-    "red": 12,
-    "green": 13,
-    "blue": 14
+  "red": 12,
+  "green": 13,
+  "blue": 14
 }
 
 def play(sets, availiable_cubes)
-    min_cubes = {
-        "red": 0,
-        "green": 0,
-        "blue": 0
-    }
-    
-    sets.each do |game|
-        cubes = game.split(",")
-        cubes.each do |cube|
-            nr_color = cube.split(" ")
-            number = nr_color[0].to_i
-            color = nr_color[1].to_sym            
-            
-            if number > availiable_cubes[color]
-                return false
-            end
-        end
+  sets.each do |game|
+    cubes = game.split(',')
+    cubes.each do |cube|
+      number, color = cube.strip.split(' ')
+
+      return false if number.to_i > availiable_cubes[color.to_sym]
     end
-    
-    true
+  end
+
+  true
 end
+
 sum = 0
-File.readlines("input2-1.txt", chomp: true).each do |line|
-    els = line.split(":")
-    id = els[0].sub("Game ", "").to_i
-    sets = els[1].split(";")
-    sum += id.to_i if play(sets, availiable_cubes)         
+File.readlines('input2-1.txt', chomp: true).each do |line|
+  els = line.split(':')
+  id = els[0].sub('Game ', '').to_i
+  sets = els[1].split(';')
+  sum += id if play(sets, availiable_cubes)
 end
 puts sum
